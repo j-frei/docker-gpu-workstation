4a15da44c2fbe9be6388eb8f4ad24ad3729ce23d
Johann Frei <johann_frei@yahoo.de>
2020-08-24 17:58:27 +0200

    Change docker runonce to use env

--- misit-tf-ssh-x11/runonce.sh
+++ misit-tf-ssh-x11/runonce.sh
@@ -2,10 +2,6 @@
 
 if [ ! -f /etc/misit-docker-runonce.lock ]; then
     echo "Container startup... Setup environment..."
-
-    if [ ! -z "$TF_FORCE_GPU_ALLOW_GROWTH" ]; then
-        echo "TF_FORCE_GPU_ALLOW_GROWTH=$TF_FORCE_GPU_ALLOW_GROWTH" >> /etc/environment
-    fi
-
+    env >> ~/.bashrc
     touch /etc/misit-docker-runonce.lock
 fi
