991baf0216897944a74cacb07f6ac154e42d384b
Johann Frei <johann_frei@yahoo.de>
2020-12-16 16:50:22 +0100

    Add latest tag to date-tagged images

--- misit-pytorch-ssh-x11/build.sh
+++ misit-pytorch-ssh-x11/build.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 tag_date=$(date '+%Y-%m-%d')
 # build image
-docker build -t misit/misit-pytorch-ssh-x11:$tag_date --pull .
+docker build -t misit/misit-pytorch-ssh-x11:latest -t misit/misit-pytorch-ssh-x11:$tag_date --pull .
