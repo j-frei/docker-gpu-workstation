c679503ad5475440ecc231bf82930f0178035341
Johann Frei <johann_frei@yahoo.de>
2020-12-16 16:43:40 +0100

    Add build date as tag

--- misit-tf-ssh-x11/build.sh
+++ misit-tf-ssh-x11/build.sh
@@ -2,5 +2,6 @@
 # move to directory of script
 cd "$(dirname "$0")"
 
+tag_date=$(date '+%Y-%m-%d')
 # build image
-docker build -t misit/misit-tf-ssh-x11:latest --pull .
+docker build -t misit/misit-tf-ssh-x11:$tag_date --pull .
--- misit-ubuntu-ssh-x11-xfce/build.sh
+++ misit-ubuntu-ssh-x11-xfce/build.sh
@@ -2,5 +2,6 @@
 # move to directory of script
 cd "$(dirname "$0")"
 
+tag_date=$(date '+%Y-%m-%d')
 # build image
-docker build -t misit/misit-ubuntu-ssh-x11-xfce:latest --pull .
+docker build -t misit/misit-ubuntu-ssh-x11-xfce:$tag_date --pull .
