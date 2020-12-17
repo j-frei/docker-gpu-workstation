1606d8e63ab1cee6c70ba187624d55b30ee3fe64
Johann Frei <johann_frei@yahoo.de>
2020-12-17 14:05:49 +0100

    Fix use of shared files for Dockerfiles

--- misit-pytorch-ssh-x11/build.sh
+++ misit-pytorch-ssh-x11/build.sh
@@ -2,6 +2,16 @@
 # move to directory of script
 cd "$(dirname "$0")"
 
+# copy shared files temporarily into dir
+for $file in "../shared/*"; do
+    cp $file $(basename $file)
+done
+
 tag_date=$(date '+%Y-%m-%d')
 # build image
 docker build -t misit/misit-pytorch-ssh-x11:latest -t misit/misit-pytorch-ssh-x11:$tag_date --pull .
+
+# cleanup files
+for $file in "../shared/*"; do
+    rm $(basename $file)
+done
