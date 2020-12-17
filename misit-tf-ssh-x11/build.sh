b1638440d08913f3330e60d0c4db61f4908effef
Johann Frei <johann_frei@yahoo.de>
2020-12-17 14:31:58 +0100

    Fix shared files copy

--- misit-tf-ssh-x11/build.sh
+++ misit-tf-ssh-x11/build.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "$0")"
 
 # copy shared files temporarily into dir
-for file in "../shared/*"; do
-    cp $file $(basename $file)
+for file in ../shared/*; do
+    cp $file ./
 done
 
 tag_date=$(date '+%Y-%m-%d')
@@ -12,6 +12,6 @@ tag_date=$(date '+%Y-%m-%d')
 docker build -t misit/misit-tf-ssh-x11:latest -t misit/misit-tf-ssh-x11:$tag_date --pull .
 
 # cleanup files
-for file in "../shared/*"; do
+for file in ../shared/*; do
     rm $(basename $file)
 done
