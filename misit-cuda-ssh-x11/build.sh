8c96b333355a827935ccb550e99ee6d76cb9e2ed
Johann Frei <johann.frei@informatik.uni-augsburg.de>
2023-07-18 10:43:05 +0200

    Update Dockerfiles

---
+++ misit-cuda-ssh-x11/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# move to directory of script
+cd "$(dirname "$0")"
+
+# copy shared files temporarily into dir
+for file in ../shared/*; do
+    cp $file ./
+done
+
+tag_date=$(date '+%Y-%m-%d')
+# build image
+docker build -t misit/misit-cuda-ssh-x11:latest -t misit/misit-cuda-ssh-x11:$tag_date --pull .
+
+# cleanup files
+for file in ../shared/*; do
+    rm $(basename $file)
+done
